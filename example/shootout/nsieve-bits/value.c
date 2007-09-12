4fb1a1ec7dd16335c00b5bcd94cb4b348c1b3edd
oskar <oskar@d3d905de-8616-4abd-9b9c-2623b5d625ba>
2007-09-12 12:56:44 +0000

    Add nsieve-bits shootout (not finished right now)
    git-svn-id: svn+ssh://svn.gna.org/svn/isaac/trunk/lisaac@21 d3d905de-8616-4abd-9b9c-2623b5d625ba

---
+++ example/shootout/nsieve-bits/value.c
@@ -0,0 +1,7 @@
+#include <stdio.h>
+main() {
+	unsigned int bbits =(sizeof(unsigned int) * 8);
+	unsigned int bsize = sizeof(unsigned int);
+	printf("bbits: %d\n",bbits);
+	printf("bsize: %d\n",bsize);
+}
