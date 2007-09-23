d0389c351cb350e735bc85cce2f269163fc9e32d
oskar <oskar@d3d905de-8616-4abd-9b9c-2623b5d625ba>
2007-09-23 20:16:10 +0000

    * Delete all .c files for cleaning * Please use .c files only in local for your own benchmarks
    git-svn-id: svn+ssh://svn.gna.org/svn/isaac/trunk/lisaac@108 d3d905de-8616-4abd-9b9c-2623b5d625ba

--- example/shootout/nsieve-bits/value.c
+++
@@ -1,7 +0,0 @@
-#include <stdio.h>
-main() {
-	unsigned int bbits =(sizeof(unsigned int) * 8);
-	unsigned int bsize = sizeof(unsigned int);
-	printf("bbits: %d\n",bbits);
-	printf("bsize: %d\n",bsize);
-}
