e338ba4232ffbf85b5d990a3b2835ccfe82fe1b0
sonntag <sonntag@d3d905de-8616-4abd-9b9c-2623b5d625ba>
2008-02-24 16:58:56 +0000

    New mode Emacs OK
    git-svn-id: svn+ssh://svn.gna.org/svn/isaac/trunk/lisaac@331 d3d905de-8616-4abd-9b9c-2623b5d625ba

--- editor/emacs/lisaac-mode.el
+++ editor/emacs/lisaac-mode.el
@@ -1,7 +1,7 @@
 ;; 
 ;;  Mode Emacs for LISAAC language 0.2.2 by Sonntag Benoit.
 ;;
-
+;;
 ;;  LICENSE
 ;;    This program is free software; you can redistribute it and/or modify
 ;;    it under the terms of the GNU General Public License as published by
@@ -15,27 +15,35 @@
 ;;
 ;;    You should have received a copy of the GNU General Public License
 ;;    along with this program.  If not, see <http://www.gnu.org/licenses/>.;
-
+;;
 ;; INSTALLATION
 ;;   To install, simply copy this file into a directory in your
 ;;   load-path and add the following two commands in your '.emacs' file:
 ;;
 ;;   (add-to-list 'auto-mode-alist '("\\.li\\'" . lisaac-mode))
 ;;   (autoload 'lisaac-mode "~/lisaac-mode" "Major mode for Lisaac Programs" t)
-
+;;
 ;; NEW FUNCTION KEY
 ;;   <tab>          : Indent current line.
-;;   <Ctrl>+C <tab> : Indent all lines.
-;;   <Ctrl>+C I     : ".if { <Cursor> };".
-;;   <Ctrl>+C E     : ".if { <Cursor> } else { };".
-;;   <Ctrl>+C U     : ".until_do { <Cursor> };".
-;;   <Ctrl>+C W     : ".while_do { <Cursor> };".
+;;   [F2]           : Indent all lines.
+;;
+;;   [F5]           : Append the Licence Header.
+;;   [F6]           : Append the Header section.
+;;   [F7]           : Append a constructor.
+;;
+;;   [F10]          : Display Previous Buffer.
+;;   [F11]          : Display the prototype pointed.
+;;   [F12]          : Vertical split window and display the prototype pointed.
+;;
 ;;   <Echap>  G     : Goto line.
-
+;;
 ;; BUG REPORT
-;;   - number: hexa, octal, ...
+;;   - Bug coloration with external `...`
 ;;   - ( + to:INTEGER;  truc.put 't' to 10; // to -> black!
-
+;;
+;; MODIFY FOR YOU :
+(defvar li-user-name "Sonntag Benoit")
+(defvar li-user-mail "sonntag@icps.u-strasbg.fr")
 
 ;;
 ;; Table abbrev.
@@ -45,7 +53,7 @@
 (define-abbrev-table 'li-mode-abbrev-table ())
 
 ;;
-;; Expression en couleur.
+;; Color Expression.
 ;;
 
 (defvar li-color 0)
@@ -59,38 +67,44 @@
 
 (defun li-message ()
   ""  
+  ;(insert (char-to-string (char-before (point))))
   (setq li-point2 (point))
   (setq li-string2 (match-string 0))
-  (end-of-line)
-  (setq li-point3 (point))
-  (setq li-string (concat "....+[^a-z0-9_]" li-string2 "\\([ \t]*,[ \t]*[a-z0-9_]*\\)*[ \t]*:[^=]"))
-
-  
-  (if (re-search-backward "^  \\(+\\|-\\)" (point-min) t 1)
-      (progn
-	(setq li-point4 (point))
-	(if (and (re-search-forward "<-\\|:=\\|?=\\|;" nil t 1)
-		 (< li-point2 (point)))
-	    (progn
-	      (goto-char li-point4)
-	      (if (re-search-forward li-string li-point3 t 1)
-		  (setq li-color 0)	  
-		(setq li-color li-face)
-		)	      
-	    )
-	  (progn
-	    (goto-char li-point4)
-	    (if (re-search-forward li-string li-point3 t 1)
-		(setq li-color 0)	  
-	      (setq li-color font-lock-function-name-face)	
-	      )
-	    )
-	  )
-	)
-    )
-
-  ;(setq li-color (get-char-property (point) 'face))
-
+  (setq li-char (char-before (- (point) (length li-string2))))
+  (if (and (>= li-char ?0) (<= li-char ?9))
+      (setq li-color font-lock-keyword-face)
+    (progn      
+      (end-of-line)
+      (setq li-point3 (point))
+      (setq li-string (concat "....+[^a-z0-9_]" li-string2 "\\([ \t]*,[ \t]*[a-z0-9_]*\\)*[ \t]*:[^=]"))
+      
+      
+      (if (re-search-backward "^  \\(+\\|-\\)" (point-min) t 1)
+          (progn
+            (setq li-point4 (point))
+            (if (and (re-search-forward "<-\\|:=\\|?=\\|;" nil t 1)
+                     (< li-point2 (point)))
+                (progn
+                  (goto-char li-point4)
+                  (if (re-search-forward li-string li-point3 t 1)
+                      (setq li-color 0)	  
+                    (setq li-color li-slot-face)
+                    )	      
+                  )
+              (progn
+                (goto-char li-point4)
+                (if (re-search-forward li-string li-point3 t 1)
+                    (setq li-color 0)	  
+                  (setq li-color font-lock-function-name-face)	
+                  )
+                )
+              )
+            )
+        )
+      
+                                        ;(setq li-color (get-char-property (point) 'face))
+      )
+    )      
   (goto-char li-point2)
   li-color
 )
@@ -102,6 +116,24 @@
       (setq li-color font-lock-warning-face)
     (setq li-color font-lock-variable-name-face)
     )
+  li-color
+)
+
+(defun li-type-color ()
+  ""  
+  ;; Detect local declaration.
+  (setq li-string2 (concat (downcase (match-string 0)) ".li" ))
+  (setq li-test 0)
+  (while (and (< li-test (length (mapcar (function buffer-name) (buffer-list))))
+              (not (string-equal (nth li-test (mapcar (function buffer-name) (buffer-list))) li-string2))
+              )
+    (setq li-test (+ li-test 1))
+    )
+  (if (< li-test (length (mapcar (function buffer-name) (buffer-list))))
+      (setq li-color li-type-face)
+    (setq li-color font-lock-type-face)
+    )
+  
   li-color
 )
 
@@ -123,6 +155,9 @@
     ;; Assignment :
     ("<-\\|:=\\|?=" 0 0 nil) 
 
+    ;; Float notation :
+    ("[0-9_]+\.[0-9]*E[+-]?[0-9]+" 0 font-lock-keyword-face nil)
+
     ;; Symbol declaration :
     ("^  \\(\\+\\|-\\)" 0 font-lock-warning-face nil)
     ("\\+\\|-" 0 (li-declaration) nil)
@@ -131,24 +166,22 @@
     ("[!@#$%^&<|=~/>?\\*\\]+" 0 font-lock-variable-name-face nil)
     
     ;; Major keywords :
-    ("^Section.*$\\|Right\\|Left\\|Expanded\\|Strict\\|Old\\|Self\\|Result\\(_[1-9]\\)?" 
+    ("^Section[ \t]+[a-zA-Z,\t 0-9_]+\\|Right\\|Left\\|Expanded\\|Strict\\|Old\\|Self\\|Result\\(_[1-9]\\)?" 
      0 font-lock-keyword-face nil)
  
     ;; Hexa-number :
-    ("[0-9]+[0-9A-F_]*h" 0 font-lock-keyword-face nil)
+    ("[0-9][0-9_]*[A-F][0-9A-F_]*h" 0 font-lock-keyword-face nil)    
 
     ;; Prototype :
-    ("[A-Z][A-Z0-9_]*" 0 font-lock-type-face nil)
+    ("[A-Z][A-Z0-9_]*" 0 (li-type-color) nil)
     
     ;; Identifier :
     ("\\.[ \t\n]*[a-z][a-z0-9_]*" 0 font-lock-function-name-face nil)
     ("[a-z_]+[a-z0-9_]*" 0 (li-message) nil)
 
     ;; Number :   
-    ("[0-9][0-9_]*[bodh]?" 0 font-lock-keyword-face nil)
-    ;("[0-9]+[0-9A-Fa-f_]*h" 0 font-lock-keyword-face nil)
-    ("[0-9_]+\.[0-9]*E[+-]?[0-9]+" 0 font-lock-keyword-face nil)
-    
+    ("[0-9][0-9_]*" 0 font-lock-keyword-face nil)
+    ("[0-9]+[0-9A-Fa-f_]*" 0 font-lock-keyword-face nil)    
     )
   "Additional expressions to highlight in Lisaac mode.")
 
@@ -200,70 +233,201 @@
 (global-set-key [end] 'end-of-line)
 (define-key li-mode-map "\t" 'li-indent-command)
 (define-key li-mode-map "\r" 'li-newline-command)
-(define-key li-mode-map "\C-c\t" 'li-indent-all-command)
-(define-key li-mode-map "\C-ci" 'li-cond1-command)
-(define-key li-mode-map "\C-ce" 'li-cond2-command)
-(define-key li-mode-map "\C-cu" 'li-until-command)
-(define-key li-mode-map "\C-cw" 'li-while-command)
+(define-key li-mode-map [f2] 'li-indent-all-command)
+(define-key li-mode-map [f5] 'li-header1-command)
+(define-key li-mode-map [f6] 'li-header2-command)
+(define-key li-mode-map [f7] 'li-header3-command)
+;(define-key li-mode-map [f8] 'li-header4-command)
+;(define-key li-mode-map [f9] 'li-while-command)
+(define-key li-mode-map [f10] 'li-previous-buffer-command)
+(define-key li-mode-map [f11] 'li-load-direct-prototype-command)
+(define-key li-mode-map [f12] 'li-load-prototype-command)
 
 ;;
 ;; Insertion loop command. 
 ;;
 
-(defun li-while-command ()
-  "Insert loop while Lisaac."
+(defun li-previous-buffer-command ()
+  "Display prototype."
   (interactive)
-  (insert ".while_do {")
+  (set-window-buffer (selected-window) (nth 0 (mapcar (function buffer-name) (buffer-list))))  
+)
+
+(defun li-load-direct-prototype-command ()
+  "Display prototype."
+  (interactive)
+
+  (if (face-equal (get-char-property (point) 'face) li-type-face)
+      (progn
+        (backward-word 1)  
+        (looking-at "[A-Z][A-Z0-9_]*")
+        (setq li-string2 (concat (downcase (match-string 0)) ".li" ))                 
+        (set-window-buffer (selected-window) li-string2)
+        )
+    )
+)
+
+(defun li-load-prototype-command ()
+  "Display prototype."
+  (interactive)
+
+  (if (face-equal (get-char-property (point) 'face) li-type-face)
+      (progn
+        (backward-word 1)  
+        (looking-at "[A-Z][A-Z0-9_]*")
+        (setq li-string2 (concat (downcase (match-string 0)) ".li" ))
+         
+        (split-window-vertically)
+        (set-window-buffer (selected-window) li-string2)
+        )
+    )
+)
+
+(defun li-header1-command ()
+  "Insert header standard Lisaac."
+  (interactive)
+  (insert "///////////////////////////////////////////////////////////////////////////////\n")
+  (setq li-string "//                             Application                                   //\n")
+  (when (string-match "isaacos" (buffer-file-name))
+    (setq li-string "//                         Isaac Opearting System                            //\n")
+    )
+  (when (string-match "lib" (buffer-file-name))
+    (setq li-string "//                             Lisaac Library                                //\n")
+    )
+  (when (string-match "src" (buffer-file-name))
+    (setq li-string "//                             Lisaac Compiler                               //\n")
+    )
+  (when (string-match "/example" (buffer-file-name))
+    (setq li-string "//                             Lisaac Example                                //\n")
+    )
+  (insert li-string)
+  (insert "//                                                                           //\n")
+  (insert "//                   LSIIT - ULP - CNRS - INRIA - FRANCE                     //\n")
+  (insert "//                                                                           //\n")
+  (insert "//   This program is free software: you can redistribute it and/or modify    //\n")
+  (insert "//   it under the terms of the GNU General Public License as published by    //\n")
+  (insert "//   the Free Software Foundation, either version 3 of the License, or       //\n")
+  (insert "//   (at your option) any later version.                                     //\n")
+  (insert "//                                                                           //\n")
+  (insert "//   This program is distributed in the hope that it will be useful,         //\n")
+  (insert "//   but WITHOUT ANY WARRANTY; without even the implied warranty of          //\n")
+  (insert "//   MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the           //\n")
+  (insert "//   GNU General Public License for more details.                            //\n")
+  (insert "//                                                                           //\n")
+  (insert "//   You should have received a copy of the GNU General Public License       //\n")
+  (insert "//   along with this program.  If not, see <http://www.gnu.org/licenses/>.   //\n")
+  (insert "//                                                                           //\n")
+  (insert "//                     http://isaacproject.u-strasbg.fr/                     //\n")
+  (insert "///////////////////////////////////////////////////////////////////////////////\n")
+)
+
+(defun li-header2-command ()
+  "Insert header standard Lisaac."
+  (interactive)
+  (insert "Section Header")
   (li-newline-command)
-  (li-newline-command)
-  (insert "}; // while_do")
-  (li-newline-command)
-  (previous-line 2)
+  (insert "\n")
+  (insert "  + name      := ")
+  (insert (upcase (truncate-string-to-width (buffer-name) (- (length (buffer-name)) 3) 0)))
+  (insert ";\n")
+  (insert "\n")
+  (insert "  - copyright := \"2003-")
+  (insert (format-time-string "%Y"))  
+  (insert " ")
+  (insert li-user-name)
+  (insert "\";\n")
+  (insert "\n")
+  (insert "  - author    := \"")
+  (insert li-user-name)
+  (insert " (")
+  (insert li-user-mail)
+  (insert ")\";\n")
+  (insert "  - comment   := \"The main prototype\";\n")
+  (insert "\n")
+  (insert "Section Inherit\n")
+  (insert "\n")
+  (insert "  - parent_object:OBJECT := OBJECT;\n")
+  (insert "\n")
+  (insert "Section Public\n")
+)
+
+(defun li-header3-command ()
+  "Insert header standard Lisaac."
+  (interactive)
+  (insert "\n  //\n")
+  (insert "  // Creation.\n")
+  (insert "  //\n")
+  (insert "\n")
+  (insert "  - create:SELF <-\n")
+  (insert "  ( + result:SELF;\n")
+  (insert "    result := clone;\n")
+  (insert "    result.make;\n")
+  (insert "    result\n")
+  (insert "  );\n")
+  (insert "\n")
+  (insert "  - make <-\n")
+  (insert "  ( \n")
+  (insert "\n")
+  (insert "  );\n")
+  (insert "\n")
+  (previous-line 3)
   (li-indent-command)
 )
 
-(defun li-until-command ()
-  "Insert loop until Lisaac."
-  (interactive)
-  (insert ".until_do {")
-  (li-newline-command)
-  (li-newline-command)
-  (insert "}; // until_do")
-  (li-newline-command)
-  (previous-line 2)
-  (li-indent-command)
-)
+;(defun li-while-command ()
+;  "Insert loop while Lisaac."
+;  (interactive)
+;  (insert ".while_do {")
+;  (li-newline-command)
+;  (li-newline-command)
+;  (insert "}; // while_do")
+;  (li-newline-command)
+;  (previous-line 2)
+;  (li-indent-command)
+;)
+
+;(defun li-until-command ()
+;  "Insert loop until Lisaac."
+;  (interactive)
+;  (insert ".until_do {")
+;  (li-newline-command)
+;  (li-newline-command)
+;  (insert "}; // until_do")
+;  (li-newline-command)
+;  (previous-line 2)
+;  (li-indent-command)
+;)
 
 ;;
 ;; Insertion test command.
 ;;
 
-(defun li-cond1-command ()
-  "Insert conditionnal Lisaac."
-  (interactive)
-  (insert ".if {")
-  (li-newline-command)
-  (li-newline-command)
-  (insert "}; // if")
-  (li-newline-command)
-  (previous-line 2)
-  (li-indent-command)
-)
+;(defun li-cond1-command ()
+;  "Insert conditionnal Lisaac."
+;  (interactive)
+;  (insert ".if {")
+;  (li-newline-command)
+;  (li-newline-command)
+;  (insert "}; // if")
+;  (li-newline-command)
+;  (previous-line 2)
+;  (li-indent-command)
+;)
 
-(defun li-cond2-command ()
-  "Insert conditionnal Lisaac."
-  (interactive)
-  (insert ".if {")
-  (li-newline-command)
-  (li-newline-command)
-  (insert "} else {")
-  (li-newline-command)
-  (li-newline-command)
-  (insert "}; // if")
-  (li-newline-command)
-  (previous-line 4)
-  (li-indent-command)
-)
+;(defun li-cond2-command ()
+;  "Insert conditionnal Lisaac."
+;  (interactive)
+;  (insert ".if {")
+;  (li-newline-command)
+;  (li-newline-command)
+;  (insert "} else {")
+;  (li-newline-command)
+;  (li-newline-command)
+;  (insert "}; // if")
+;  (li-newline-command)
+;  (previous-line 4)
+;  (li-indent-command)
+;)
 
 
 (defvar li-count-all 0)
@@ -306,8 +470,10 @@
 (defvar li-indent-base 0)
 
 (require 'font-lock)
-(defvar li-face		'li-face
-  "Face to use for LaTeX major keywords.")
+(defvar li-slot-face		'li-slot-face
+  "Face to use for slot major.")
+(defvar li-type-face		'li-type-face
+  "Face to use for type file.")
 
 (defun li-indent-previous ()
   "Indent with previous line."
@@ -391,19 +557,19 @@
     (setq li-point (+ li-point 1))
   )
 
-  ; On redescend: Ligne courante.
+  ; Next line: Current Line.
   (while (/= li-count-line 0)
     (setq li-count-line (- li-count-line 1))
     (next-line 1))
   
-  ; Fin de la ligne.
+  ; Go to End of line.
   (end-of-line)
 
-  ; Initialisation.
+  ; Initialization.
   (setq li-indent-2 0) 
   (setq li-point (point))
 
-  ; On compte les parantheses () or {} or [], de la ligne courante.
+  ; Count () or {} or [], of current line.
   (setq li-test 0)
   (while (= li-test 0)
     (setq li-point (- li-point 1))
@@ -456,16 +622,16 @@
     )
   )
 
-  ; On supprime tous les espaces.
+  ; Deleted spaces
   (beginning-of-line)
   (delete-horizontal-space)
   
-  ; On ajoute l'indentation
+  ; Append indentation.
   (setq li-indent (- li-indent li-indent-2))
   (setq li-indent (+ li-indent li-indent-base))
   (indent-to-column li-indent)
 
-  ; On se met en fin de ligne.
+  ; Go to end of line.
   (end-of-line)
 ) 
 
@@ -518,7 +684,7 @@
 ;(auto-overlay-load-definition
 ;'latex
 ;'(self ("\\$" (priority . 3) (face . (background-color . "green")))))
-(autoload 'latex-mode "Mode Latex" "jfh" t)
+;(autoload 'latex-mode "Mode Latex" "jfh" t)
 ;(require 'font-latex)
 
   ;; compatibility MS-DOS
@@ -541,28 +707,31 @@
   (make-local-variable 'font-lock-defaults)
 
   ; Creation new face.
-  (make-face 'li-face)
-  (set-face-foreground 'li-face "blue")
-  ;(set-face-underline-p 'li-face t)
-  ;(set-face-italic-p 'li-face t)
-  (set-face-bold-p 'li-face t)
-  ;(set-face-background 'li-string-face "black")
+  (make-face 'li-slot-face)
+  (set-face-foreground 'li-slot-face "blue") 
+  ;(set-face-bold-p 'li-slot-face t)
+  ;(set-face-italic-p 'li-slot-face t)  
+  (set-face-underline-p 'li-slot-face t)
+  ;(set-face-background 'li-slot-face "black")
+
+  (make-face 'li-type-face)
+  (copy-face font-lock-type-face 'li-type-face)
+  (set-face-underline-p 'li-type-face t)
 
   ;(setq font-lock-string-face '(li-font-lock-string))
   (setq font-lock-defaults '(li-font-lock-keywords))
   
-
-  ;; Ne pas remplacer les espaces par des tabulations
+  ;; No replace spaces by tabulations
   (setq-default indent-tabs-mode nil)
 
-  ; Pour pouvoir utiliser la molette de la souris.
+  ; For use mouse Wheel.
   (require 'mwheel)
   (mwheel-install)
 
   (run-hooks 'li--mode-hook))
 
 ;;
-;; Fin du mode Lisaac.
+;; End of Lisaac mode.
 ;;
 (provide 'lisaac-mode)
 
