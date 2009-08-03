06ec4b50eaed60086377ad9d0e8453e428d6b550
Mildred Ki'Lya <silkensedai@online.fr>
2009-08-04 01:40:55 +0200

    Updated example/compile.sh

--- example/compile.sh
+++ example/compile.sh
@@ -1,3 +1,20 @@
 #!/bin/bash
 
-for i in `find -name "*.li"` ; do cat $i | grep -q "main" && echo Compile $i && ../src2/lisaac $i ; done
+lisaac=$1
+args=
+shift
+if [ -z "$lisaac" ]; then
+  lisaac=lisaac
+  args="-O -q"
+fi
+
+#for i in `find -name "*.li"` ; do cat $i | grep -q "main" && echo Compile $i && lisaac $i -O -q ; done
+
+find -name "*.li" | xargs grep -- '- main *<-' | cut -d: -f1 | while read li; do
+
+  echo "$lisaac $li $@ $args"
+  "$lisaac" "$li" "$@" $args
+  res=$?
+  [ $res != 0 ] && echo "Error $res"
+
+done
