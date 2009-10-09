c2d6262a2ccbc2199d34623b193f9622a37e7b16
Mildred Ki'Lya <silkensedai@online.fr>
2009-10-10 01:16:40 +0200

    Updated tests. LIP files are now used to describe tests.

--- run-tests.sh
+++ run-tests.sh
@@ -70,6 +70,21 @@ header(){
   fi
 }
 
+test-patterns(){
+  while read pattern; do
+    [ -z "$pattern" ] && continue
+    if ! egrep -- "$pattern" <<<"$2" >/dev/null; then
+      echo " - Expected:    $pattern"
+      echo
+      echo "$2"
+      return 1
+    else
+      echo " - Match:       $pattern"
+    fi
+  done <<<"$1"
+  return 0
+}
+
 if $op_runcwd; then
   res=1
   echo
@@ -77,6 +92,53 @@ if $op_runcwd; then
   if [ -f test.sh -a -x test.sh ]; then
     ./test.sh
     res=$?
+  elif [ -f make.lip ]; then
+    lisaac -test-description 2>&1
+    args=(-test)
+    if ! lisaac -test-run 2>&1; then
+      args=("${args[@]}" -partial)
+      partial=true
+    else
+      partial=false
+    fi
+    compile="$(lisaac "${args[@]}" -o result.exe 2>&1)"
+    res=$?
+    echo
+    if [ $res != 0 ]; then
+      echo " - Compilation: $res"
+    else
+      echo " - Compilation: ok"
+    fi
+    if ! lisaac -test-compile; then
+      res=0
+    fi
+    if [ $res != 0 ]; then
+      echo
+      echo "$compile"
+    else
+      compile_patterns="$(lisaac -test-compile-patterns 2>&1)"
+      test-patterns "$compile_patterns" "$compile"
+      res=$?
+      if ! $partial && [ $res = 0 ]; then
+        run="$(./result.exe)"
+        res=$?
+        if [ $res != 0 ]; then
+          echo " - Execution:   $res"
+        else
+          echo " - Execution:   ok"
+        fi
+        run_patterns="$(lisaac -test-run-patterns 2>&1)"
+        test-patterns "$run_patterns" "$run"
+        res2=$?
+        if [ $res = 0 ]; then
+          res=$res2
+        fi
+        if [ $res2 = 0 ]; then
+          echo
+          echo "$run"
+        fi
+      fi
+    fi
   else
     echo "Unknown test: $op_testname"
   fi
